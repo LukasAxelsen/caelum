436b7269a82fb9aeee3c61388caab1d04ce558d5
lukas <lukasaccelsen@gmail.com>
2025-08-28 22:14:25 +0800

    While adding employee, create user ID and update user ID are set the same value. This issue is now avoided by dynamically get the ID of the current user logged in. Authorization based on JWT.

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -66,6 +67,8 @@ public class EmployeeServiceImpl implements EmployeeService {
      */
     @Override
     public void save(EmployeeDTO employeeDTO) {
+        System.out.println("当前线程ID: " + Thread.currentThread().getId());
+
         Employee employee = new Employee();
         // Copy object properties
         BeanUtils.copyProperties(employeeDTO, employee);
@@ -77,9 +80,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         // Set create user ID and update user ID
-        // TODO 后期需要改为当前登录用户的ID
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
